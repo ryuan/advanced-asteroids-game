44dd11f483ee5be4a9979e2556e908cf5b0624f6
Ruoda Yuan <ruoda.yuan@gmail.com>
2021-10-02 14:43:01 -0500

    E2_20 finished

---
+++ src/main/java/edu/uchicago/gerber/_01control/E2_20.java
@@ -0,0 +1,18 @@
+package edu.uchicago.gerber._01control;
+
+/**
+ * E2_20: Prints out a Christmas tree - Merry Very Early Christmas!
+ */
+
+public class E2_20 {
+    public static void main(String[] args) {
+        System.out.println("   /\\   ");
+        System.out.println("  /  \\  ");
+        System.out.println(" /    \\ ");
+        System.out.println("/      \\");
+        System.out.println("--------");
+        System.out.println("  \"  \"  ");
+        System.out.println("  \"  \"  ");
+        System.out.println("  \"  \"  ");
+    }
+}
