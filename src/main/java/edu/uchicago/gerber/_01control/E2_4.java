344f454326ed415707fdc901912b8ccdfa72d0a0
Ruoda Yuan <ruoda.yuan@gmail.com>
2021-10-02 04:28:39 -0500

    Played around with adding comments

--- src/main/java/edu/uchicago/gerber/_01control/E2_4.java
+++ src/main/java/edu/uchicago/gerber/_01control/E2_4.java
@@ -1,15 +1,26 @@
 package edu.uchicago.gerber._01control;
 import java.util.Scanner;
 
+/**
+ * E2_4: This program takes two integer inputs from the user and prints out a variety of calculated values.
+ */
+
 public class E2_4 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        /*
+        Ask user to input two integers and store them in distinct variables
+         */
         System.out.print("Please enter your first integer: ");
         int num1 = in.nextInt();
 
         System.out.print("Please enter your second integer: ");
         int num2 = in.nextInt();
+
+        /*
+        Calculate and print out the sum, difference, product, average, distance, max, and min.
+         */
 
         int sum = num1 + num2;
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
