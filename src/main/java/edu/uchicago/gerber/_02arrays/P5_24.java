d9a238c7b4e4b4fb737394b9a70aff66786ecd7f
Ruoda Yuan <ruoda.yuan@gmail.com>
2021-10-10 22:15:29 -0500

    Completed P5_24

---
+++ src/main/java/edu/uchicago/gerber/_02arrays/P5_24.java
@@ -0,0 +1,54 @@
+package edu.uchicago.gerber._02arrays;
+import java.util.Scanner;
+
+/**
+ *  P5_24: Given a year in Roman numeral form, convert it into integer format.
+ */
+public class P5_24 {
+    public static int value(char r) {
+        if (r == 'I')
+            return 1;
+        if (r == 'V')
+            return 5;
+        if (r == 'X')
+            return 10;
+        if (r == 'L')
+            return 50;
+        if (r == 'C')
+            return 100;
+        if (r == 'D')
+            return 500;
+        if (r == 'M')
+            return 1000;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        /*
+        Prompt user for input year, checking to make sure it is valid for Roman numeral conversion
+         */
+        String rnInput;
+        do {
+            System.out.print("Enter a Roman numeral you want to convert to integer form: ");
+            rnInput = in.nextLine();
+        }
+        while (rnInput.length() == 0);
+
+        int total = 0;
+        while (rnInput.length() > 0) {
+            if (rnInput.length() == 1 || value(rnInput.charAt(0)) >= value(rnInput.charAt(1))) {
+                total += value(rnInput.charAt(0));
+                rnInput = rnInput.substring(1);
+            }
+            else {
+                int diff = value(rnInput.charAt(1)) - value(rnInput.charAt(0));
+                total += diff;
+                rnInput = rnInput.substring(2);
+            }
+        }
+
+        System.out.println(total);
+    }
+}
