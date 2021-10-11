c6af52ca800adbb1ff745b10e1bc506757e4251f
Ruoda Yuan <ruoda.yuan@gmail.com>
2021-10-10 22:58:36 -0500

    Completed P6_9

---
+++ src/main/java/edu/uchicago/gerber/_02arrays/E6_9.java
@@ -0,0 +1,23 @@
+package edu.uchicago.gerber._02arrays;
+
+/**
+ * E6_9: Checks for equality between two arrays.
+ */
+public class E6_9 {
+    public static boolean equals(int[] a, int[] b) {
+        if (a.length != b.length) {return false;}
+        else {
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] != b[i]) {return false;}
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] x = {4, 5, 6, 7, 10};
+        int[] y = {4, 5, 6, 7, 10};
+
+        System.out.println(equals(x,y));
+    }
+}
