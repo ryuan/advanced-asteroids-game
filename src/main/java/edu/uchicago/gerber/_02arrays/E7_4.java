98a3de30b064bbf2d7d6a7d23d7f33998177198f
Ruoda Yuan <ruoda.yuan@gmail.com>
2021-10-11 00:30:58 -0500

    Completed E7_4

---
+++ src/main/java/edu/uchicago/gerber/_02arrays/E7_4.java
@@ -0,0 +1,32 @@
+package edu.uchicago.gerber._02arrays;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * E7_4: Prompts user for input & output text files, prints each line with index prefix, then writes to file.
+ */
+public class E7_4 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner console = new Scanner(System.in);
+        System.out.print("Input file: ");
+        String inputFileName = console.next();
+        System.out.print("Output file: ");
+        String outputFileName = console.next();
+
+        File inputFile = new File(inputFileName);
+        Scanner in = new Scanner(inputFile);
+        PrintWriter out = new PrintWriter(outputFileName);
+
+        int counter = 1;
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            out.print("/*" + counter + "*/ " + line);
+            counter += 1;
+        }
+
+        in.close();
+        out.close();
+    }
+}
